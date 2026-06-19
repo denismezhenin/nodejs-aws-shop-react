574ebe41abe00717917eeb9e30c6935a0d44ce14
denismezhenin <103366013+denismezhenin@users.noreply.github.com>
2026-06-19 17:05:36 +0200

    task-9

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://gyl4e2imdk.execute-api.eu-central-1.amazonaws.com/dev",
-  order: "https://sebo2r2gn3p446wnpqqzro2b5u0wdrwt.lambda-url.eu-central-1.on.aws/api/profile/cart",
+  order: "http://denismezhenin-cart-api-prod.eu-central-1.elasticbeanstalk.com/api/profile/cart",
   import: "https://exp9la2o5g.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://sebo2r2gn3p446wnpqqzro2b5u0wdrwt.lambda-url.eu-central-1.on.aws/api",
+  cart: "http://denismezhenin-cart-api-prod.eu-central-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
--- src/index.tsx
+++ src/index.tsx
@@ -27,12 +27,7 @@ const queryClient = new QueryClient({
   },
 });
 
-if (import.meta.env.DEV) {
-  const { worker } = await import("./mocks/browser");
-  worker.start({ onUnhandledRequest: "bypass" });
-}
 
-console.log("new deployment");
 const container = document.getElementById("app");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
