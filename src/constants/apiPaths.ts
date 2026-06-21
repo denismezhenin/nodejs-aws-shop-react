780afaddcb156d43fdf0c119979959cc89cf9292
denismezhenin <103366013+denismezhenin@users.noreply.github.com>
2026-06-21 19:52:45 +0200

    task-10

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
   product: "https://gyl4e2imdk.execute-api.eu-central-1.amazonaws.com/dev",
-  order: "http://denismezhenin-cart-api-prod.eu-central-1.elasticbeanstalk.com/api/profile/cart",
+  order: "http://denismezhenin-cart-api-prod.eu-central-1.elasticbeanstalk.com/api/profile/cart)",
   import: "https://exp9la2o5g.execute-api.eu-central-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "http://denismezhenin-bff-api-dev.eu-central-1.elasticbeanstalk.com",
   cart: "http://denismezhenin-cart-api-prod.eu-central-1.elasticbeanstalk.com/api",
 };
 
